78eb542df2a21c267ad2371f2d764254906a0ebc
Alena Nilava <44751670+AlenaNilava@users.noreply.github.com>
2019-03-19 21:28:04 +0300

    Adding web service tests.

---
+++ WebServiceTests/APITests.cs
@@ -0,0 +1,52 @@
+﻿namespace TestWebService
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TestWebService.APIUtils;
+
+    [TestClass]
+    public class APITests
+    {
+        [TestMethod]
+        public void TestGet()
+        {
+            // Arrange
+            string endPointName = "users";
+            int expectedCode = 200;
+
+            // Act
+            var actualResult = APICalls.ExecuteGetAndGetStatusCode(endPointName);
+
+            // Assert
+            Assert.AreEqual(expectedCode, actualResult, "Status code is not the same");
+        }
+
+        [TestMethod]
+        public void TestGetHeader()
+        {
+            // Arrange
+            string endPointName = "users";
+            string expectedHeader = "application/json; charset=utf-8";
+
+            // Act
+            var actualResult = APICalls.ExecuteGetAndGetContentTypeHeader(endPointName);
+
+            // Assert
+            Assert.AreEqual(expectedHeader, actualResult, "Content-type header is not the same");
+        }
+
+        [TestMethod]
+        public void TestGetBody()
+        {
+            // Arrange
+            string endPointName = "users";
+            int expectedUsersCount = 10;
+
+            // Act
+            var actualResult = APICalls.ExecuteGetAndGetUsersCount(endPointName);
+
+            // Assert
+            Assert.AreEqual(expectedUsersCount, actualResult, "Users count is not the same");
+        }
+    }
+}
+
---
+++ WebServiceTests/APIUtils/APICalls.cs
@@ -0,0 +1,56 @@
+﻿namespace TestWebService.APIUtils
+{
+    using System.Net;
+    using System.Reflection;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    public class APICalls
+    {
+        static readonly string serverUri = "https://jsonplaceholder.typicode.com/";
+
+        public static int ExecuteGetAndGetStatusCode(string endpointName)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadString(serverUri + endpointName);
+                return GetStatusCode(client);
+            }
+        }
+
+        public static string ExecuteGetAndGetContentTypeHeader(string endpointName)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadString(serverUri + endpointName);
+                return client.ResponseHeaders.Get("Content-Type");
+            }
+        }
+
+        public static int ExecuteGetAndGetUsersCount(string endpointName)
+        {
+            using (var client = new WebClient())
+            {
+                var responseString = client.DownloadString(serverUri + endpointName);
+                JArray json = JsonConvert.DeserializeObject<JArray>(responseString);
+                return json.Count;
+            }
+        }
+
+        private static int GetStatusCode(WebClient client)
+        {
+            FieldInfo responseField = client.GetType().GetField("m_WebResponse", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            if (responseField != null)
+            {
+                HttpWebResponse response = responseField.GetValue(client) as HttpWebResponse;
+
+                if (response != null)
+                {
+                    return (int)response.StatusCode;
+                }
+            }
+            return 0;
+        }
+    }
+}
